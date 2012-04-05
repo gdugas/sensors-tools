52230e9bbe88e1c2c3dc485e563c4f79874c57a7
remy.d1 <devnull@localhost>
2012-04-05 17:11:31 +0200

    another little update

---
+++ bin/showalldb.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import os,sys
+import sqlite3 
+
+dirpyfile = os.path.dirname(sys.argv[0])
+#print dirpyfile
+
+wdir = os.path.abspath( os.path.dirname(sys.argv[0]) + '/../' )
+sys.path.append(wdir)
+
+#print wdir
+import sensorsConf
+
+#mydatabase="history.db"
+connection=sqlite3.connect(sensorsConf.DB_name)
+cursor=connection.cursor()
+cursor.execute('SELECT * FROM temp;')
+print cursor.fetchall()
+cursor.execute('SELECT COUNT(*) FROM temp;')
+print str(cursor.fetchall())+ ' enregistrements'
+#connection.commit() #only for changes on database....
+cursor.close()
+quit()
